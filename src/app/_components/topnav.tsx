456fb0f2c95a1498046644a31d3983faf8616e15
Erik S. Carlsten <ecarlste@gmail.com>
2025-01-16 15:55:30 -0700

    enable clerk sign in / user buttons

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,24 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  UserButton,
+} from "@clerk/nextjs";
+
+export function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>Mech Lab</div>
+
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
