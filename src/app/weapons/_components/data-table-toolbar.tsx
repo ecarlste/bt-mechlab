69f074443971aaead5a983651e5252585254bc79
Erik S. Carlsten <ecarlste@gmail.com>
2025-01-21 17:34:58 -0700

    weapons table new button goes to new weapon page

--- src/app/weapons/_components/data-table-toolbar.tsx
+++ src/app/weapons/_components/data-table-toolbar.tsx
@@ -6,12 +6,15 @@ import { Input } from "~/components/ui/input";
 import { DataTableViewOptions } from "./data-table-view-options";
 import { SquarePlus } from "lucide-react";
 import { Button } from "~/components/ui/button";
+import { useRouter } from "next/navigation";
 
 interface DataTableToolbarProps<TData> {
   table: Table<TData>;
 }
 
 export function DataTableToolbar<TData>({ table }: DataTableToolbarProps<TData>) {
+  const router = useRouter();
+
   return (
     <div className="flex items-center justify-between">
       <Input
@@ -21,7 +24,7 @@ export function DataTableToolbar<TData>({ table }: DataTableToolbarProps<TData>)
         className="h-8 w-[150px] lg:w-[250px]"
       />
       <div className="flex items-center space-x-2">
-        <Button variant="outline" size="sm" className="ml-auto flex h-8" onClick={() => console.log("Add new weapon")}>
+        <Button variant="outline" size="sm" className="ml-auto flex h-8" onClick={() => router.push("/weapons/new")}>
           <SquarePlus />
           New
         </Button>
