207b6a7a3f4ff0f396bb2ba7925d1ece0e6e6417
Erik S. Carlsten <ecarlste@gmail.com>
2025-01-22 00:01:29 -0700

    remove unneeded db calls on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,4 @@
-import { isUserAdmin } from "~/data/auth";
-import { getAllWeapons } from "~/data/weapon-dto";
-
-export const dynamic = "force-dynamic";
-
 export default async function HomePage() {
-  const weaponList = await getAllWeapons();
-
-  const isAdmin = await isUserAdmin();
-
   return (
     <main className="flex flex-1 items-center justify-center">
       <div className="shrink text-6xl">Welcome to the Mech Lab</div>
